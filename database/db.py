f48eb7acf3d1b1400eca2e6ce22b2d3849fe1128
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-07-19 21:29:06 +0500

    changed db_url for server

--- database/db.py
+++ database/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
-DATABASE_URL = "postgresql://postgres:77girado@db/testbot"
+DATABASE_URL = "postgresql://botadmin:77girado@localhost/testbot"
 
 engine = create_engine(DATABASE_URL, echo=True)
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
