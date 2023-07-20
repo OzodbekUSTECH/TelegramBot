8a11fe78eadaf517d6985c7350cd38846504437e
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-07-20 15:45:03 +0500

    get back commented part of the code

--- app/auth/router.py
+++ app/auth/router.py
@@ -125,78 +125,78 @@ async def update_admin_data(admin_id: int, admin_update: AdminUpdateSchema, curr
 
     return db_admin
 
-# @router.get('admin/{admin_id}', name="get admin by id", response_model = AdminSchema)
-# async def get_admin_by_id(admin_id: int, db: Session = Depends(get_db)):
-#     # if not current_user.is_superuser:
-#     #     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="You are not a superuser")
-#     db_admin = db.query(models.Admin).filter(models.Admin.id == admin_id).first()
-#     if not db_admin:
-#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found. dont exist")
-#     return db_admin
+@router.get('admin/{admin_id}', name="get admin by id", response_model = AdminSchema)
+async def get_admin_by_id(admin_id: int, db: Session = Depends(get_db)):
+    # if not current_user.is_superuser:
+    #     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="You are not a superuser")
+    db_admin = db.query(models.Admin).filter(models.Admin.id == admin_id).first()
+    if not db_admin:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found. dont exist")
+    return db_admin
 
 
-# @router.put("/admin/me", response_model=AdminSchema)
-# async def change_own_data(background_tasks: BackgroundTasks,new_data = UpdateOwnAdminSchema, current_user=Depends(get_current_user), db: Session = Depends(get_db)):
-#     db_admin = db.query(models.Admin).filter(models.Admin.id == current_user.id).first()
-#     if not db_admin:
-#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="You are not an admin")
-#     prev_data = db_admin
-#     if new_data.email is not None:
-#         db_admin.email = new_data.email
-#     if new_data.username is not None:
-#         db_admin.username = new_data.username
-#     if new_data.first_name is not None:
-#         db_admin.first_name = new_data.first_name
-#     if new_data.last_name is not None:
-#         db_admin.last_name = new_data.last_name
-#     if new_data.phone_number is not None:
-#         db_admin.phone_number = new_data.phone_number
-#     if new_data.channel_id is not None:
-#         db_admin.channel_id = new_data.channel_id
+@router.put("/admin/me", response_model=AdminSchema, tags=['Own DATA'])
+async def change_own_data(background_tasks: BackgroundTasks,new_data = UpdateOwnAdminSchema, current_user=Depends(get_current_user), db: Session = Depends(get_db)):
+    db_admin = db.query(models.Admin).filter(models.Admin.id == current_user.id).first()
+    if not db_admin:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="You are not an admin")
+    prev_data = db_admin
+    if new_data.email is not None:
+        db_admin.email = new_data.email
+    if new_data.username is not None:
+        db_admin.username = new_data.username
+    if new_data.first_name is not None:
+        db_admin.first_name = new_data.first_name
+    if new_data.last_name is not None:
+        db_admin.last_name = new_data.last_name
+    if new_data.phone_number is not None:
+        db_admin.phone_number = new_data.phone_number
+    if new_data.channel_id is not None:
+        db_admin.channel_id = new_data.channel_id
 
-#     db.commit()
-#     db.refresh(db_admin)
+    db.commit()
+    db.refresh(db_admin)
 
-#     super_users = db.query(models.Admin).filter(models.Admin.is_superuser == True, models.Admin.tg_id != current_user.tg_id).all()
+    super_users = db.query(models.Admin).filter(models.Admin.is_superuser == True, models.Admin.tg_id != current_user.tg_id).all()
 
-#     async def send_message_task():
+    async def send_message_task():
         
-#         message_text = (
-#             "Данные были изменены:\n\n"
-#             f'Номер Админа: {db_admin.id}\n'
-#             f'ID tg: {db_admin.tg_id}\n'
-#             )
-#         has_changes = False  # Variable to track if there are any changes
+        message_text = (
+            "Данные были изменены:\n\n"
+            f'Номер Админа: {db_admin.id}\n'
+            f'ID tg: {db_admin.tg_id}\n'
+            )
+        has_changes = False  # Variable to track if there are any changes
         
-#         # Helper function to check if a field is updated and add it to the message_text
-#         def add_change(field_name, prev_value, new_value):
-#             nonlocal message_text, has_changes
-#             if prev_value != new_value:
-#                 message_text += f"{field_name}:\n{prev_value} => {new_value}\n"
-#                 has_changes = True
+        # Helper function to check if a field is updated and add it to the message_text
+        def add_change(field_name, prev_value, new_value):
+            nonlocal message_text, has_changes
+            if prev_value != new_value:
+                message_text += f"{field_name}:\n{prev_value} => {new_value}\n"
+                has_changes = True
 
-#         add_change("email", prev_data.email, db_admin.email)
-#         add_change("username", prev_data.username, f"@{db_admin.username}")
-#         add_change("Имя", prev_data.first_name, db_admin.first_name)
-#         add_change("Фамилия", prev_data.last_name, db_admin.last_name)
-#         add_change("Номер телефона", prev_data.phone_number, db_admin.phone_number)
-#         add_change("ID канала", prev_data.channel_id, db_admin.channel_id)
+        add_change("email", prev_data.email, db_admin.email)
+        add_change("username", prev_data.username, f"@{db_admin.username}")
+        add_change("Имя", prev_data.first_name, db_admin.first_name)
+        add_change("Фамилия", prev_data.last_name, db_admin.last_name)
+        add_change("Номер телефона", prev_data.phone_number, db_admin.phone_number)
+        add_change("ID канала", prev_data.channel_id, db_admin.channel_id)
 
-#         # If no changes, just display the data without any indication of change
-#         if not has_changes:
-#             message_text += (
-#                 f'Номер Админа: {db_admin.id}\n'
-#                 f'ID tg: {db_admin.tg_id}\n'
-#             )
+        # If no changes, just display the data without any indication of change
+        if not has_changes:
+            message_text += (
+                f'Номер Админа: {db_admin.id}\n'
+                f'ID tg: {db_admin.tg_id}\n'
+            )
 
-#         btns = types.InlineKeyboardMarkup()
-#         close_msg_button = types.InlineKeyboardButton("Скрыть", callback_data=f"close_msg")
-#         btns.add(close_msg_button)
+        btns = types.InlineKeyboardMarkup()
+        close_msg_button = types.InlineKeyboardButton("Скрыть", callback_data=f"close_msg")
+        btns.add(close_msg_button)
         
-#         await bot.send_message(chat_id=current_user.tg_id, text=message_text, reply_markup=btns, parse_mode="HTML")
-#         for superuser in super_users:
-#             await bot.send_message(chat_id=superuser.tg_id, text=message_text, reply_markup=btns, parse_mode="HTML")
+        await bot.send_message(chat_id=current_user.tg_id, text=message_text, reply_markup=btns, parse_mode="HTML")
+        for superuser in super_users:
+            await bot.send_message(chat_id=superuser.tg_id, text=message_text, reply_markup=btns, parse_mode="HTML")
 
-#     background_tasks.add_task(send_message_task)
+    background_tasks.add_task(send_message_task)
 
-#     return db_admin
+    return db_admin
