27edd22e0db9ba93daae63cc978d6e54bd535d73
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-07-20 13:07:02 +0500

    solved i hope)

--- telegram/channelManager/paginationbtns.py
+++ telegram/channelManager/paginationbtns.py
@@ -25,10 +25,13 @@ def get_list_of_all_channels_statistics(curr_page, all_channels, channel = None,
     nextbtn = types.InlineKeyboardButton(text="➡️", callback_data="next_channel")
     if channel is not None and link_channel is not None:
         kb.add(closemsg, channel_link).add(backbtn, counter_text, nextbtn)
-    else:
+    elif channel is not None and link_channel is None:
+        
         change_channel_id = types.InlineKeyboardButton(text="Изменить ", web_app=WebAppInfo(url=f"https://gazoblok-bukhara.uz/api/v1/admin/{channel.id}/channel")) #ссылка на измение айди канала 
 
         kb.add(closemsg, change_channel_id).add(backbtn, counter_text, nextbtn)
+    else:
+        kb.add(closemsg).add(backbtn, counter_text, nextbtn)
 
     return kb
 
